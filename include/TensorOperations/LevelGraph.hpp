f5d78f6e8321b9601f96698855d2c361d29543ae
Lucas Sawade <lsawade@princeton.edu>
2026-09-14 11:39:25 -0400

    CPU safe "compilation"

--- include/TensorOperations/LevelGraph.hpp
+++ include/TensorOperations/LevelGraph.hpp
@@ -712,6 +712,7 @@ struct LevelGraph {
 
   bool index_consistent() const {
     return index_consistent_impl(
+        std::index_sequence<>{},
         std::make_index_sequence<Impl::lg_total_members_v<LevelsT>>{});
   }
 
